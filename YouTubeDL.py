20c411d000a70a6dc441cffa7a50dc6c37b3b212
Nu55k0pF <madironbrain@googlemail.com>
2026-03-11 12:46:17 +0100

    added download audio func

--- YouTubeDL.py
+++ YouTubeDL.py
@@ -17,6 +17,18 @@ def download_video(url, save_path):
     except Exception as e:
         print(f"Error: {e}")
 
+def download_audio(url, save_path):
+    try:
+        yt = YouTube(url)
+        ys = yt.streams.filter(only_audio=True).first()
+        print(f"Downloading: {yt.title}")
+        ys.download(save_path)
+        print("Download completed!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
 if __name__ == "__main__":
     url = input("Enter the YouTube video URL: ")
     save_path = SAVE_PATH
@@ -24,10 +36,3 @@ if __name__ == "__main__":
         save_path = input("Enter the path where the video will be saved: ")
         os.makedirs(save_path)
     download_video(url, save_path)
-
-
-
-
-# url = "https://www.youtube.com/watch?v=9bZkp7q19f0"
-# yt = YouTube(url, on_progress_callback=on_progress)
-# print(yt.title)
